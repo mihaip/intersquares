8c5306513f1e45fd51f8e8a655f34641ee2d08a6
Mihai Parparita <mihaip@chromium.org>
2011-09-17 16:11:48 -0700

    Add basic timezone support. Bucket intersections by date.

--- app/data/checkins.py
+++ app/data/checkins.py
@@ -2,6 +2,8 @@ import calendar
 import datetime
 import logging
 
+import pytz
+
 import base.interval_tree
 
 HERE_NOW_DELTA = datetime.timedelta(hours=2)
@@ -64,12 +66,32 @@ class Checkin(object):
     self.timezone = json_data.get('timeZone', None)
     self.venue_id = json_data.get('venue', {}).get('id', None)
     self.venue_name = json_data.get('venue', {}).get('name', None)
-    self.timestamp = datetime.datetime.utcfromtimestamp(json_data['createdAt'])
+    self.timestamp = datetime.datetime.utcfromtimestamp(
+        json_data['createdAt']).replace(tzinfo = pytz.UTC)
 
     # TODO(mihaip): shout, comments, photos, overlaps
 
   def should_use(self):
     return self.type == 'checkin' and not self.private and self.venue_id
+
+  def get_local_timestamp(self):
+    if self.timezone:
+      tzinfo = pytz.timezone(self.timezone)
+      if tzinfo:
+        return self.timestamp.astimezone(tzinfo)
+    return self.timestamp
+
+  def display_year(self):
+    return self.get_local_timestamp().strftime('%Y')
+
+  def display_date(self):
+    return self.get_local_timestamp().strftime('%B %d, %Y')
+
+  def display_short_date(self):
+    return self.get_local_timestamp().strftime('%B %d')
+
+  def display_time(self):
+    return self.get_local_timestamp().strftime('%H:%M')
 
 class Checkins(object):
   def __init__(self):
--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+import os
+import sys
+
 from google.appengine.dist import use_library
 use_library('django', '1.2')
+
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'third_party'))
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
